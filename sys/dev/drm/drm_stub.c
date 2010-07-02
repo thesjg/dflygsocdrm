a9477e6cb13dad7c9f016c8ac985ab839e997ea6
David Shao <davshao@gmail.com>
2010-07-01 22:39:43 -0700

    synch drm_ioctl b

--- sys/dev/drm/drm_stub.c
+++ sys/dev/drm/drm_stub.c
@@ -190,7 +190,7 @@ static void drm_master_destroy(struct kref *kref)
 #ifdef __linux__
 	kfree(master);
 #else
-	DRM_INFO("pid (%d), minor_id (%d) master destroyed",
+	DRM_INFO("pid (%d), minor_id (%d) master destroyed\n",
 		DRM_CURRENTPID, master->minor->index);
 	free(master, DRM_MEM_STUB);
 #endif /* __linux__ */
