76b5af36d6e0ed0a5dd6350b8a61e41c71ab8eac
David Shao <davshao@gmail.com>
2010-07-16 17:08:35 -0700

    changed tsleep to finite wait in radeon_cp.c

--- sys/dev/drm/radeon/radeon_cp.c
+++ sys/dev/drm/radeon/radeon_cp.c
@@ -1763,35 +1763,33 @@ void radeon_do_release(struct drm_device * dev)
 			if ((dev_priv->flags & RADEON_FAMILY_MASK) >= CHIP_R600) {
 				while ((ret = r600_do_cp_idle(dev_priv)) != 0) {
 					DRM_DEBUG("radeon_do_cp_idle %d\n", ret);
+#if 0
 					tsleep_interlock(&dev->lock.lock_queue,
 							 PCATCH);
-#ifndef DRM_NEWER_LOCK
-//                			DRM_UNLOCK();
-#endif
 					ret = tsleep(&dev->lock.lock_queue,
 						    PCATCH | PINTERLOCKED,
 						    "rdnrel", 0);
-#ifndef DRM_NEWER_LOCK
-//                			DRM_LOCK();
 #endif
-/* DragonFly equivalent of
- *					mtx_sleep(&ret, &dev->dev_lock, 0,
- *					    "rdnrel", 1);
- */
+#ifdef __linux__
+					schedule();
+#else
+					tsleep(&ret, PZERO, "rdnrel", 1);
+#endif
 				}
 			} else {
 				while ((ret = radeon_do_cp_idle(dev_priv)) != 0) {
+#if 0
 					DRM_DEBUG("radeon_do_cp_idle %d\n", ret);
 					tsleep_interlock(&dev->lock.lock_queue,
 							 PCATCH);
-#ifndef DRM_NEWER_LOCK
-//                			DRM_UNLOCK();
-#endif
 					ret = tsleep(&dev->lock.lock_queue,
 						    PCATCH | PINTERLOCKED,
 						    "rdnrel", 0);
-#ifndef DRM_NEWER_LOCK
-//                			DRM_LOCK();
+#endif
+#ifdef __linux__
+					schedule();
+#else
+					tsleep(&ret, PZERO, "rdnrel", 1);
 #endif
 				}
 			}
