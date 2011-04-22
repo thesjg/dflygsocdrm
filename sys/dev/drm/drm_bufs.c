e456467a0f9aa0f8ac78ffe638746f0696bf45f6
David Shao <davshao@gmail.com>
2011-04-22 14:45:34 -0700

    debug foff in drm_vm.c b revert

--- sys/dev/drm/drm_bufs.c
+++ sys/dev/drm/drm_bufs.c
@@ -2012,12 +2012,10 @@ int drm_mapbufs(struct drm_device *dev, void *data,
 	vaddr = round_page((vm_offset_t)vms->vm_daddr + MAXDSIZ);
 
 #ifndef __linux__
-	DRM_INFO("vm_mmap() before: vaddr (%016lx), size (%016lx), foff (%016lx), pid (%d), uid (%u)\n",
+	DRM_INFO("vm_mmap() before: vaddr (%016lx), size (%016lx), foff (%016lx)\n",
 		(unsigned long)vaddr,
 		(unsigned long)size,
-		foff,
-		DRM_CURRENTPID,
-		DRM_CURRENTUID);
+		foff);
 #endif
 
 #if defined(__DragonFly__)
